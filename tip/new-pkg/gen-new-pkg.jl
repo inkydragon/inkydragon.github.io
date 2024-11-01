c67743b83c65e6623715a1c2c9613d217538438c
Chengyu HAN <cyhan.dev@outlook.com>
2024-11-01 15:25:36 +0800

    tip: +gen-new-pkg.jl

---
+++ tip/new-pkg/gen-new-pkg.jl
@@ -0,0 +1,38 @@
+# https://github.com/JuliaCI/PkgTemplates.jl
+
+using PkgTemplates
+
+# Template Plugins
+plugins = [
+    Git(;
+        # ignore=String[],
+        branch="main",
+        jl=true,  # add a .jl suffix to the remote URL.
+        # gpgsign=false,
+    ),
+    License(; name="MIT", destination="LICENSE"),
+    Tests(;
+        project=true,
+        # aqua=false,
+        # aqua_kwargs=NamedTuple(),
+        # jet=false,
+    ),
+
+    # CI
+    Dependabot(),
+    GitHubActions(; extra_versions=["1.10", "pre"],),
+    Documenter{GitHubActions}(),
+]
+
+tpl = Template(;
+    # User Options
+    user="inkydragon",
+    authors=["Chengyu HAN <cyhan.dev@outlook.com> and contributors"],
+    # Package Options
+    dir=".",
+    # Template Plugins
+    plugins=plugins,
+    interactive=false,
+)
+
+tpl("NewPkg.jl")
