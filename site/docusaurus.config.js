b426f8837f6a7d95f671d7260e1d1cedfee1873c
Chengyu HAN <cyhan.dev@outlook.com>
2025-05-11 20:56:33 +0800

    site: fix editUrl

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/sig-a11y/memo/tree/main/site/',
+          editUrl: 'https://github.com/inkydragon/inkydragon.github.io/tree/main/site/',
         },
         blog: {
           showReadingTime: true,
@@ -56,7 +56,7 @@ const config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/sig-a11y/memo/tree/main/site/',
+          editUrl: 'https://github.com/inkydragon/inkydragon.github.io/tree/main/site/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
